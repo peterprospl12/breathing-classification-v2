19dba06ab157f42a46156e776b2dbadd452d2f7c
Tomasz Sankowski <sankowski.tomek@gmail.com>
2025-09-18 00:14:21 +0200

    Rollback to old transformer flow

--- breathing_model/model/transformer/inference/transform.py
+++ breathing_model/model/transformer/inference/transform.py
@@ -7,22 +7,20 @@ from breathing_model.model.transformer.utils import DataConfig
 class MelSpectrogramTransform:
     def __init__(self, config: DataConfig):
         self.config = config
-        self.transform = torchaudio.transforms.MFCC(
+        self.transform = torchaudio.transforms.MelSpectrogram(
             sample_rate=config.sample_rate,
-            n_mfcc=config.n_mels,
-            melkwargs={
-                'n_fft': config.n_fft,
-                'hop_length': config.hop_length,
-                'n_mels': config.n_mels,
-                'center': True,
-                'power': 2.0
-            }
+            n_fft=config.n_fft,
+            hop_length=config.hop_length,
+            n_mels=config.n_mels
         )
+        self.db_transform = torchaudio.transforms.AmplitudeToDB(stype='power')
 
     def __call__(self, signal: np.ndarray) -> torch.Tensor:
-        if signal.ndim > 1:  # stereo -> mono
+        if signal.ndim > 1:
             signal = signal.mean(axis=1)
-        waveform = torch.tensor(signal, dtype=torch.float32).unsqueeze(0)  # [1, samples]
-        mfcc = self.transform(waveform)  # [1, n_mfcc, T]
-        mfcc = mfcc.unsqueeze(0)  # [1, 1, n_mfcc, T]
-        return mfcc
+
+        waveform = torch.tensor(signal).unsqueeze(0)
+        mel = self.transform(waveform)
+        mel = self.db_transform(mel)
+        mel = mel.unsqueeze(0)
+        return mel
